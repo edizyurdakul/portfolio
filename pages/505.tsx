c9e8bec3707c73ab2d08f8b5d5f03c0997a4e2ef
Ediz <53665610+edizyurdakul@users.noreply.github.com>
2022-02-15 14:07:58 +0200

    only load local font tailwindcss, delete 500 error page

--- pages/505.tsx
+++
@@ -1,3 +0,0 @@
-export default function NotFound() {
-  return <div>500 Page</div>;
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Switzer-Variable", "Helvetica", "Arial", "sans-serif"],
+        sans: ["Switzer-Variable"],
       },
     },
   },
