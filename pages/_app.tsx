548460768554f33d1bafa35bd7dcd09b97b67a17
Ediz <53665610+edizyurdakul@users.noreply.github.com>
2022-03-22 02:09:00 +0200

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,36 @@
 import "../styles/globals.css";
+import Script from "next/script";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-8Q6S7X5L8N`}
+      />
+      <Script
+        id="google-tag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-8Q6S7X5L8N');
+      `,
+        }}
+      />
+      <ThemeProvider attribute="class">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 }
 
